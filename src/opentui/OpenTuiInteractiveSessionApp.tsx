f83a154916ebb390b5b741405766bc2c64805a82
noQils <daffaaqilmahmud@gmail.com>
2026-05-28 18:30:39 +0700

    fix: center NOQ logo in empty state

--- src/opentui/OpenTuiInteractiveSessionApp.tsx
+++ src/opentui/OpenTuiInteractiveSessionApp.tsx
@@ -415,11 +415,14 @@ function TranscriptEntry(props: {
 function EmptyTranscriptState(props: { isCompact: boolean }) {
   return (
     <box
+      flexGrow={1}
       backgroundColor={openTuiTheme.color.canvas}
       paddingX={1}
       paddingY={props.isCompact ? 0 : 1}
       flexDirection="column"
       gap={props.isCompact ? 0 : 1}
+      justifyContent="center"
+      alignItems="center"
     >
       <box flexDirection="row" gap={1}>
         <text fg={openTuiTheme.color.teal} selectable={false}>
